3c896e1a4a0661261e5afd37ad851649b0e7f13b
Nat <wmainvielle@gmail.com>
2023-05-09 10:56:24 +0200

    update docker

--- src/database.ts
+++ src/database.ts
@@ -7,9 +7,10 @@ dotenv.config();
 const models = [UserModel, TarotCardModel, UserTarotCardModel];
 
 const database = new Sequelize({
-  username: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
+  username: process.env.PG_USER || 'postgres',
+  password: process.env.PG_PASSWORD || 'azerty',
+  database: process.env.PG_DATABASE || 'api_natchy',
+  host: process.env.PG_HOST || 'localhost',
   dialect: 'postgres',
 
   define: { underscored: true },
